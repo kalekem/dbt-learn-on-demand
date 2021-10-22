9ee3825559a1f088cd7ec6901fd9d127c0d160e8
Cleophas <kalekemcleophas01@gmail.com>
2021-10-22 11:48:51 -0400

    Dbt on demand (#2)
    * model and test commit
    
    * sources commit
    
    Co-authored-by: Cleophas <cleophas.kalekem@slalom.com>

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -5,7 +5,11 @@ with customers as (
         first_name,
         last_name
 
+<<<<<<< HEAD
+    from {{ source('jaffle_shop', 'customers')}}
+=======
     from raw.jaffle_shop.customers
+>>>>>>> 51263835e1cdaed1f87f37319847000bd27d9596
 
 )
 select * from customers
